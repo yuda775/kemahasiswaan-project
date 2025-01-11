0013e7434a09714204f3c0de01f69292051d74ce
yuda775 <saputra.yuda775@gmail.com>
2025-01-11 22:38:27 +0700

    initial commit

---
+++ src/layout/AppTopbar.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { useLayout } from '@/layout/composables/layout';
+import AppConfigurator from './AppConfigurator.vue';
+
+const { toggleMenu, toggleDarkMode, isDarkTheme } = useLayout();
+</script>
+
+<template>
+    <div class="layout-topbar">
+        <div class="layout-topbar-logo-container">
+            <button class="layout-menu-button layout-topbar-action" @click="toggleMenu">
+                <i class="pi pi-bars"></i>
+            </button>
+            <router-link to="/" class="layout-topbar-logo">
+                <img alt="logo" src="/images/logo-amik.png" style="width: 2.5rem" />
+                <span class="text-lg font-bold">STMIK AMIK BANDUNG</span>
+            </router-link>
+        </div>
+
+        <div class="layout-topbar-actions">
+            <div class="layout-config-menu">
+                <button type="button" class="layout-topbar-action" @click="toggleDarkMode">
+                    <i :class="['pi', { 'pi-moon': isDarkTheme, 'pi-sun': !isDarkTheme }]"></i>
+                </button>
+                <div class="relative">
+                    <button
+                        v-styleclass="{ selector: '@next', enterFromClass: 'hidden', enterActiveClass: 'animate-scalein', leaveToClass: 'hidden', leaveActiveClass: 'animate-fadeout', hideOnOutsideClick: true }"
+                        type="button"
+                        class="layout-topbar-action layout-topbar-action-highlight"
+                    >
+                        <i class="pi pi-palette"></i>
+                    </button>
+                    <AppConfigurator />
+                </div>
+            </div>
+
+            <button
+                class="layout-topbar-menu-button layout-topbar-action"
+                v-styleclass="{ selector: '@next', enterFromClass: 'hidden', enterActiveClass: 'animate-scalein', leaveToClass: 'hidden', leaveActiveClass: 'animate-fadeout', hideOnOutsideClick: true }"
+            >
+                <i class="pi pi-ellipsis-v"></i>
+            </button>
+
+            <div class="layout-topbar-menu hidden lg:block">
+                <div class="layout-topbar-menu-content">
+                    <button type="button" class="layout-topbar-action">
+                        <i class="pi pi-calendar"></i>
+                        <span>Calendar</span>
+                    </button>
+                    <button type="button" class="layout-topbar-action">
+                        <i class="pi pi-inbox"></i>
+                        <span>Messages</span>
+                    </button>
+                    <button type="button" class="layout-topbar-action">
+                        <i class="pi pi-user"></i>
+                        <span>Profile</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
